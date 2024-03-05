16ff0ee051ae3503b9a8e1db90ed2c36b28f8f30
1123Alessandro <alessandroandyrei@gmail.com>
2024-03-05 19:53:54 +0800

    working value setters

--- test.py
+++ test.py
@@ -1,10 +1,36 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-service = webdriver.ChromeService('./chromedriver.exe')
+# WINDOWS
+# service = webdriver.ChromeService('./chromedriver.exe')
+
+# LINUX
+service = webdriver.ChromeService('./chromedriver-linux64/chromedriver')
+
 options = webdriver.ChromeOptions()
 options.add_argument('--start-maximized')
 
+# LINUX
+# options.add_argument('--headless')
+options.binary_location = './chrome-linux64/chrome'
+
 driver = webdriver.Chrome(service=service, options=options)
+
+driver.get('https://google.com')
+
+tb = driver.find_element(By.XPATH, '//*[@id="APjFqb"]')
+tb.send_keys('testing')
+print(tb.get_property('value'))
+driver.execute_script('arguments[0].value = "hatdog"', tb)
+print(tb.get_property('value'))
+tb.send_keys(Keys.ENTER)
+
+searched = driver.find_element(By.XPATH, '//*[@id="APjFqb"]')
+
+print(searched.get_property('value'))
+
 time.sleep(5)
-driver.quit()
+print('time for bed')
+# driver.quit()
