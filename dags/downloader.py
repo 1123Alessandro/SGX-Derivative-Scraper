4816e647024c3e41b51fc02f8eeb7cede2d41d70
1123Alessandro <alessandroandyrei@gmail.com>
2024-03-07 05:42:13 +0800

    impelemented option select to types

--- dags/downloader.py
+++ dags/downloader.py
@@ -47,18 +47,10 @@ def fetch_data():
 
     # time.sleep(3)
     new_dates = option_select(driver, '//*[@id="page-container"]/template-base/div/div/section[1]/div/sgx-widgets-wrapper/widget-research-and-reports-download[1]/widget-reports-derivatives-tick-and-trade-cancellation/div/sgx-input-select[2]/sgx-select-model')
-
     driver.execute_script(f"arguments[0]._options = {new_dates['entries']}", new_dates['element'])
 
-    types = driver.find_element(By.XPATH, '//*[@id="page-container"]/template-base/div/div/section[1]/div/sgx-widgets-wrapper/widget-research-and-reports-download[1]/widget-reports-derivatives-tick-and-trade-cancellation/div/sgx-input-select[1]/sgx-select-model')
-
-    entries = types.get_property('_options')
-    # for i in entries:
-    # i['selected'] = False
-
-    raw = json.dumps(entries)
-
-    driver.execute_script(f"arguments[0]._options = {raw}", types)
+    new_types = option_select(driver, '//*[@id="page-container"]/template-base/div/div/section[1]/div/sgx-widgets-wrapper/widget-research-and-reports-download[1]/widget-reports-derivatives-tick-and-trade-cancellation/div/sgx-input-select[1]/sgx-select-model', 'Tick Data Structure')
+    driver.execute_script(f"arguments[0]._options = {new_types['entries']}", new_types['element'])
 
     button = driver.find_element(By.XPATH, '//*[@id="page-container"]/template-base/div/div/section[1]/div/sgx-widgets-wrapper/widget-research-and-reports-download[1]/widget-reports-derivatives-tick-and-trade-cancellation/div/button')
     driver.execute_script('arguments[0].click()', button)
