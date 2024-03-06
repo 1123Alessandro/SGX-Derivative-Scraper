7aacb43f05fd351aa5216739e02bd1065bc286b5
1123Alessandro <alessandroandyrei@gmail.com>
2024-03-07 04:34:30 +0800

    draft downloader

--- dags/dag.py
+++ dags/dag.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from downloader import fetch_data
 
 def testing():
-    return 'hello world';
+    return datetime.now()
 
 dag = DAG(
     'SGX-Derivatives',
@@ -12,14 +13,14 @@ dag = DAG(
         'retries': 1,
         'retry_delay': timedelta(seconds=30),
     },
-    start_date = datetime(2024, 3, 6),
+    start_date = datetime(2024, 3, 5),
     schedule = timedelta(minutes=10),
     catchup = False,
 )
 
 a = PythonOperator(
     task_id = 'entrance',
-    python_callable = testing,
+    python_callable = fetch_data,
     dag=dag
 )
 
