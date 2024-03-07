43eff668026b0d0821b80863e82ab87adad2c440
1123Alessandro <alessandroandyrei@gmail.com>
2024-03-07 15:26:48 +0800

    working renamer

--- dags/renamer.py
+++ dags/renamer.py
@@ -1,5 +1,6 @@
 import os
 import re
+from datetime import datetime
 
 def rename_downloads():
     dl_dir = os.path.join(os.getcwd(), 'diels')
@@ -8,8 +9,11 @@ def rename_downloads():
     for dir, subd, file in os.walk(dl_dir):
         for f in file:
             m = re.search('(?P<fn>TickData_structure|TC_structure)\.dat', f)
-            print('AAAAAAAAAAAAAAAAAAAAAAAAAAA ==================================================')
-            print(m.group('fn'))
+            filename = m.group('fn')
+            date = datetime.now()
+            strdate = date.strftime('%Y%m%d')
+            new_fn = f"{filename}_{strdate}"
+            os.rename(f"{dl_dir}/{filename}.dat", f"{dl_dir}/{new_fn}.dat")
 
 
 if __name__ == '__main__':
